b745d2ab0b35dcc63f189578392bfce57450bdd2
Bharath Veerakumar <80382298+bharathveerakumar@users.noreply.github.com>
2022-09-08 16:58:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,94 +1,80 @@
 #Global Variables
 TOKEN=''
 HEADER=''
+FINAL=[] #Streaming responses from all resouces were stored in this FINAL list
 
+fieldSelector=['Pod', 'StatefulSet']
 
-#Filterations
-nodeFilter=[
-    'TerminatedAllPods', 'NodeNotSchedulable', 'CIDRNotAvailable', 
-    'RegisteredNode', 'Starting', 'KubeletSetupFailed', 
-    'RemovingNode', 'NodeSelectorMismatching', 'InsufficientFreeCPU',
-    'DeletingNode', 'CIDRAssignmentFailed', 'FailedMount',
-    'DeletingAllPods', 'InsufficientFreeMemory', 'OutOfDisk',
-    'TerminatingEvictedPod', 'HostNetworkNotSupported', 'NilShaper',
-    'NodeReady', 'Rebooted', 'NodeHasSufficientDisk',
-    'NodeNotReady', 'NodeOutOfDisk', 'InvalidDiskCapacity',
-    'NodeSchedulable', 'FreeDiskSpaceFailed'
-]
-podFilter=[
-    'Pulling', 'Pulled', 'Failed',
-    'InspectFailed', 'ErrImageNeverPull', 'BackOff',
-    'Created', 'Started', 'Killing',
-    'Unhealthy', 'FailedSync', 'FailedValidation',
-    'OutOfDisk', 'HostPortConflict'
-]
-rcFilter=[
-    'SuccessfulCreate', 'FailedCreate', 'SuccessfulDelete', 'FailedDelete'
-]
+import threading
+import time
 
 
 #Endurl and paths
-endpoint='https://kubernetes'   #http://localhost:8080
-apiPath='/apis/events.k8s.io/v1/events?watch=true' #or /api/v1/watch/events
+endpoint='https://kubernetes/apis/events.k8s.io/v1/events?watch=true&fieldSelector=regarding.kind='   #http://localhost:8080
+# endpoint='http://localhost:8080/apis/events.k8s.io/v1/events?watch=true&fieldSelector=regarding.kind='
 
 
-class Events:
-    def __init__(self):
-        self.nodeFilter=nodeFilter
-        self.podFilter=podFilter
-        self.rcFilter=rcFilter
-        self.send_request()
+def get_token():
+    try:
+        import os   
+        global TOKEN, HEADER
+        if(os.path.isfile('/var/run/secrets/kubernetes.io/serviceaccount/token')):
+            fileOPEN=open('/var/run/secrets/kubernetes.io/serviceaccount/token', 'r')
+            fileRead=fileOPEN.read()
+            TOKEN=fileRead.rstrip()
+            fileOPEN.close()
+            HEADER={
+                'Authorization': 'Bearer '+str(TOKEN)
+            }
+    except Exception as e:
+        print(e)
 
 
-    def get_token(self):
+class Events(threading.Thread):
+    def __init__(self, fs):
         try:
-            import os   
-            global TOKEN, HEADER
-            if(os.path.isfile('/var/run/secrets/kubernetes.io/serviceaccount/token')):
-                fileOPEN=open('/var/run/secrets/kubernetes.io/serviceaccount/token', 'r')
-                fileRead=fileOPEN.read()
-                TOKEN=fileRead.rstrip()
-                fileOPEN.close()
-                HEADER={
-                    'Authorization': 'Bearer '+str(TOKEN)
-                }
+            threading.Thread.__init__(self)
+            self.fs=fs
+        except Exception as e:
+            print(e)
+
+
+    def run(self):
+        try:
+            self.send_request()
         except Exception as e:
             print(e)
 
 
     def send_request(self):
         try:
-            self.get_token()
             import requests
             import json
             req=requests.Session()
-            with req.get(endpoint+apiPath, headers=HEADER, verify=False, stream=True) as res:
+            with req.get(endpoint+self.fs, headers=HEADER, verify=False, stream=True) as res:
                 for line in res.iter_lines():
                     if(line):
                         lineJSON=json.loads(line)
                         lineJSON=json.dumps(lineJSON, indent=4)
-                        self.filter_events(lineJSON)
+                        lock=threading.Lock()
+                        lock.acquire()
+                        print("This Thread is runs for " +self.fs, time.ctime(time.time()))
+                        FINAL.append(lineJSON)
+                        print(lineJSON)
+                        lock.release()
         except Exception as e:
             print(e)
-        
 
-    def filter_events(self, lineJSON):
-        try:
-            keys=lineJSON.keys()
-            if "object" in keys:
-                kind=lineJSON["object"]["regarding"]["kind"]
-                reason=lineJSON["object"]["reason"]
-                
-                if kind=="Pod" and reason in self.podFilter:
-                    print(lineJSON)
-                elif kind=="Node" and reason in self.nodeFilter:
-                    print(lineJSON)
-                elif kind=="ReplicationController" and reason in self.rcFilter:
-                    print(lineJSON)   
-
-        except Exception as e:
-            print(e)
 
 
 if __name__=='__main__':
-    eve=Events()
+    try:
+        get_token()
+        threadObject=[]
+        for i in fieldSelector:
+            insTemp=Events(i)
+            threadObject.append(insTemp)
+        for i in threadObject:
+            i.start()
+    except Exception as e:
+        print(e)
